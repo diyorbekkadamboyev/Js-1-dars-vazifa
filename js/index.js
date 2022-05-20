e971265a3ddc4d5d7f95288e7ce8529880ba62ee
Diyorbek03 <dqadamboyev117@gmail.com>
2022-05-20 21:30:15 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var name = prompt("Ismingizni kiriting");
+var year = prompt("Yilingizni kiriting");
+var from = prompt("Qayerda tug'ulgansiz");
+var job = prompt("Kasbingiz");
+var educationalCenter = prompt("O'qiydigan joyingizni ko'rsating");
+
+console.log("Ismi: " + name + "\n" + "Yoshi: " + (2022 - year) + "\n" + "Tug'ulgan joyi: " + from + "\n" + "Kasbi: " + job + "\n" + "O'qiydigan joyi: " + educationalCenter);
+
+
+
+
+
+
